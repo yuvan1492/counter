9366c3c52f394a0b866876e22088466466d3ceec
YuvanShankar <107350619+yuvan1492@users.noreply.github.com>
2022-10-26 22:37:30 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+let count =0;
+const value=document.querySelector('#span');
+const btns=document.querySelectorAll('.btn');
+btns.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+        const style = e.currentTarget.classList;
+        if(style.contains('decrease')){
+            count--;
+        }
+        else if(style.contains('increase')){
+            count++;
+        }
+        else{
+            count=0;
+        }
+        if(count>0)
+        {
+            value.style.color='green';
+        }
+        else if(count<0){
+            value.style.color='red';
+        }
+        else{
+            value.style.color='black';
+        }
+        value.textContent = count;
+    });
+
+});
